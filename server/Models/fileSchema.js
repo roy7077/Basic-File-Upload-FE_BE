cd41d8d6faf0477c302238864d76cb6e955cab28
Sagar ray <raysagar807@gmail.com>
2024-03-27 03:35:22 +0530

    added mailer functionality

--- server/Models/fileSchema.js
+++ server/Models/fileSchema.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose');
+const nodemailer = require("nodemailer");
 const fileSchema=new mongoose.Schema({
     name:{
         type:String,
@@ -12,6 +13,34 @@ const fileSchema=new mongoose.Schema({
     },
     email:{
         type:String,
+    }
+})
+
+fileSchema.post("save",async function(doc){
+    try{
+        const transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth: {
+              user: process.env.MAIL_USER,
+              pass: process.env.MAIL_PASS,
+            },
+          });
+
+          const info = await transporter.sendMail({
+            from: 'Sagar ray', // sender address
+            to: doc.email, // list of receivers
+            subject: "File Uploaded Successfully", // Subject line
+            text: "Your file uploaded successfully", // plain text body
+            html: ` <body>
+                    <p>Your file has been uploaded successfully.</p>
+                    <p>View your file <a href="${doc.imageUrl}">here</a>.</p>
+                    </body>`
+          });
+        
+          console.log(info);
+    }
+    catch(error){
+        console.log(error);
     }
 })
 
